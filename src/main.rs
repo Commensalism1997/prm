0c9a19b8680c088de8aacfd118ff7099d9c004ca
Commensalism <folouindustries@protonmail.com>
2026-05-28 17:31:09 +0300

    Some improvements

--- src/main.rs
+++ src/main.rs
@@ -36,25 +36,24 @@ fn main() -> Result<ExitCode, Box<dyn std::error::Error>> {
             Some(n) => countpb.set_message(format!("Deleting {}...", n.to_string_lossy())),
             None => countpb.set_message("Deleting...")
         }
-        let p = PathBuf::from(ipath);
-        if p.is_dir() {
+        if ipath.is_dir() {
             let spinner = mpb.add(style::themed_spinner()).with_message("Analyzing...");
             spinner.enable_steady_tick(Duration::from_millis(100));
-            let entry_count = count_dir(&p)? + 1;
+            let entry_count = count_dir(&ipath)? + 1;
             let pb = mpb.add(style::themed_progressbar_no_msg(entry_count as u64));
-            full_remove_dir(&p, &pb, &spinner, cli.verbose, &mpb)?;
+            full_remove_dir(&ipath, &pb, &spinner, cli.verbose, &mpb)?;
             pb.finish_and_clear();
             spinner.finish_with_message("Finished");
             spinner.finish_and_clear();
         }
         else {
-            let pfname = p.file_name();
+            let pfname = ipath.file_name();
             let spinner = mpb.add(style::themed_spinner());
             match pfname {
                 Some(n) => spinner.set_message(format!("Deleting {}...", n.to_string_lossy())),
                 None => spinner.set_message("Deleting file...")
             }
-            fs::remove_file(&p)?;
+            fs::remove_file(&ipath)?;
             if cli.verbose {
                 match pfname {
                     Some(n) => mpb.println(format!("Removed file {}", n.to_string_lossy()))?,
@@ -70,7 +69,7 @@ fn main() -> Result<ExitCode, Box<dyn std::error::Error>> {
     Ok(if success { ExitCode::SUCCESS } else { ExitCode::FAILURE })
 }
 
-fn count_dir(path: &Path) -> Result<usize, Box<dyn std::error::Error>> {
+fn count_dir(path: impl AsRef<Path>) -> Result<usize, Box<dyn std::error::Error>> {
     let readdir = fs::read_dir(path)?;
     let mut count: usize = 0;
     for e in readdir {
@@ -87,12 +86,10 @@ fn count_dir(path: &Path) -> Result<usize, Box<dyn std::error::Error>> {
 
 fn full_remove_dir(path: &Path, pb: &ProgressBar, spinner: &ProgressBar, verbose: bool, mpb: &MultiProgress) -> Result<(), Box<dyn std::error::Error>> {
     let readdir = fs::read_dir(path)?;
-    let mut dirs: Vec<fs::DirEntry> = Vec::new();
     for e in readdir {
         let e = e?;
         if e.file_type()?.is_dir() {
             full_remove_dir(&e.path(), pb, spinner, verbose, mpb)?;
-            dirs.push(e);
             continue;
         }
         spinner.set_message(format!("Removing file {}...", e.file_name().to_string_lossy()));
